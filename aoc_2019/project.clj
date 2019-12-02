6c866f2ac85e83c4f01af174b749e204a3001678
Paul Evans <pevans@pauls-mbp.lan>
2019-12-01 22:44:05 -0500

    initial commit

---
+++ aoc_2019/project.clj
@@ -0,0 +1,5 @@
+(defproject aoc_2019 "0.1.0-SNAPSHOT"
+  :description "Advent of Code Solutions"    
+  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :repl-options {:init-ns aoc-2019.core})
+
---
+++ aoc_2019/src/aoc_2019/core.clj
@@ -0,0 +1,45 @@
+(ns aoc-2019.core
+  (:require [clojure.test :refer :all]
+            [clojure.java.io :refer [resource]])
+  (:import (java.io BufferedReader StringReader)))
+
+(defn required-fuel
+  [mass]
+  (- (Math/floor (/ mass 3)) 2))
+
+(defn required-fuel-with-fuel
+  [mass]
+  (loop [sum 0 remaining-mass mass]
+    (if (<= remaining-mass 0)
+      sum
+      (let [reqd-fuel-val (required-fuel remaining-mass)]
+        (recur
+         (if (> reqd-fuel-val 0)
+           (+ sum reqd-fuel-val)
+           sum)
+         reqd-fuel-val)))))
+
+(defn- sum-all
+  [reqd-fuel-fn]
+  (reduce (fn [sum val]
+            (+ sum (reqd-fuel-fn (Integer/parseInt val))))
+          0
+          (line-seq (BufferedReader. (StringReader. (slurp (resource "input_1.txt")))))))
+
+(defn sum-all-required-fuel
+  []
+  (sum-all required-fuel))
+
+(defn sum-all-required-fuel-with-fuel
+  []
+  (sum-all required-fuel-with-fuel))
+
+(defn -main [& args]
+  (is (== 2 (required-fuel 12)))
+  (is (== 2 (required-fuel 12)))
+  (is (== 654 (required-fuel 1969)))
+  (is (== 33583 (required-fuel 100756))))
+
+
+
+
