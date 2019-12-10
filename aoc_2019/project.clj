e73f0d74dae6f7186abf7f26bc46e54bb0ae7a47
Paul Evans <evansp2@gmail.com>
2019-12-10 17:13:27 -0500

    some solved

--- aoc_2019/project.clj
+++ aoc_2019/project.clj
@@ -1,5 +1,6 @@
 (defproject aoc_2019 "0.1.0-SNAPSHOT"
   :description "Advent of Code Solutions"    
   :dependencies [[org.clojure/clojure "1.10.0"]]
-  :repl-options {:init-ns aoc-2019.core})
+  :repl-options {:init-ns aoc-2019.core}
+  :jvm-opts ["-Xmx2g"])
 
