ce0b52c27f7b6cf99a093da25a578f8dae450885
Paul Evans <evansp2@gmail.com>
2019-12-13 17:02:36 -0500

    finished day 7

--- aoc_2019/src/aoc_2019/core.clj
+++ aoc_2019/src/aoc_2019/core.clj
@@ -42,8 +42,8 @@
   (is (== 33583 (required-fuel 100756))))
 
 (defn intcode
-  [ints]  
-  (loop [result ints i 0]
+  [program]  
+  (loop [result program i 0]
     (let [opcode (nth result i)
           op (case opcode
                1 +
@@ -61,8 +61,8 @@
                  (+ i 4)))))))
 
 (defn intcode-csv
-  [ints-csv]
-  (let [result (intcode (vec (map #(Integer/parseInt %) (str/split ints-csv #","))))]    
+  [program-csv]
+  (let [result (intcode (vec (map #(Integer/parseInt %) (str/split program-csv #","))))]    
     (reduce #(str %1 "," %2) (first result) (rest result))))
 
 ;; orig input 1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,9,1,19,1,19,5,23,1,23,6,27,2,9,27,31,1,5,31,35,1,35,10,39,1,39,10,43,2,43,9,47,1,6,47,51,2,51,6,55,1,5,55,59,2,59,10,63,1,9,63,67,1,9,67,71,2,71,6,75,1,5,75,79,1,5,79,83,1,9,83,87,2,87,10,91,2,10,91,95,1,95,9,99,2,99,9,103,2,10,103,107,2,9,107,111,1,111,5,115,1,115,2,119,1,119,6,0,99,2,0,14,0
@@ -291,10 +291,11 @@
     1 [0 0 0]))
 
 (defn param-val
+  "Returns the value of a parameter based on its mode (position or immediate)."
   [inputs i param-mode offset]
   (if (== param-mode 0)               
-    (nth inputs (nth inputs (+ i offset)))
-    (nth inputs (+ i offset))))
+    (nth inputs (nth inputs (+ i offset))) ; position mode
+    (nth inputs (+ i offset))))            ; immediate mode
 
 (defn perform-op
   [inputs i op param-1-mode param-2-mode]
@@ -303,61 +304,57 @@
          (op (param-val inputs i param-1-mode 1)
              (param-val inputs i param-2-mode 2))))
 
-(defn perform-jump
-  [inputs i test param-1-mode param-2-mode]
-  )
-
 (defn intcode-5
-  [inputs input]  
-  (loop [inputs inputs
+  [program input]  
+  (loop [program program
          i 0 ; instruction pointer
          terminate false]
     (when (not terminate)      
-      (let [opcode-str (str (nth inputs i))
+      (let [opcode-str (str (nth program i))
             opcode-chars (seq opcode-str)]        
         (cond
           (str/ends-with? opcode-str "1") ; add
           (let [[param-1-mode param-2-mode] (param-modes opcode-chars)]
-            (recur (perform-op inputs i + param-1-mode param-2-mode)
+            (recur (perform-op program i + param-1-mode param-2-mode)
                    (+ i 4)
                    false))
           
           (str/ends-with? opcode-str "2") ; mult
           (let [[param-1-mode param-2-mode] (param-modes opcode-chars)]
-            (recur (perform-op inputs i * param-1-mode param-2-mode)
+            (recur (perform-op program i * param-1-mode param-2-mode)
                    (+ i 4)
                    false))
 
           (str/ends-with? opcode-str "3") ; read an input val          
-          (recur (assoc inputs (nth inputs (+ i 1)) input)
+          (recur (assoc program (nth program (+ i 1)) input)
                  (+ i 2)
                  false)
 
           (str/ends-with? opcode-str "4") ; write
           (let [[param-1-mode] (param-modes opcode-chars)]            
-            (print (param-val inputs i param-1-mode 1) " ")
-            (recur inputs (+ i 2) false))
+            (print (param-val program i param-1-mode 1) " ")
+            (recur program (+ i 2) false))
 
           (str/ends-with? opcode-str "5") ; jump-if-true
           (let [[param-1-mode param-2-mode] (param-modes opcode-chars)]
-            (let [jump-test (param-val inputs i param-1-mode 1)]
+            (let [jump-test (param-val program i param-1-mode 1)]
               (if (not (== jump-test 0))
-                (recur inputs (param-val inputs i param-2-mode 2) false)
-                (recur inputs (+ i 3) false))))          
+                (recur program (param-val program i param-2-mode 2) false)
+                (recur program (+ i 3) false))))          
 
           (str/ends-with? opcode-str "6") ; jump-if-false
           (let [[param-1-mode param-2-mode] (param-modes opcode-chars)]
-            (let [jump-test (param-val inputs i param-1-mode 1)]
+            (let [jump-test (param-val program i param-1-mode 1)]
               (if (== jump-test 0)
-                (recur inputs (param-val inputs i param-2-mode 2) false)
-                (recur inputs (+ i 3) false))))          
+                (recur program (param-val program i param-2-mode 2) false)
+                (recur program (+ i 3) false))))          
 
           (str/ends-with? opcode-str "7") ; less than
           (let [[param-1-mode param-2-mode] (param-modes opcode-chars)]            
-            (recur (assoc inputs
-                          (nth inputs (+ i 3))
-                          (if (< (param-val inputs i param-1-mode 1)
-                                 (param-val inputs i param-2-mode 2))
+            (recur (assoc program
+                          (nth program (+ i 3))
+                          (if (< (param-val program i param-1-mode 1)
+                                 (param-val program i param-2-mode 2))
                             1
                             0))
                    (+ i 4)
@@ -365,10 +362,10 @@
 
           (str/ends-with? opcode-str "8") ; equals
           (let [[param-1-mode param-2-mode] (param-modes opcode-chars)]            
-            (recur (assoc inputs
-                          (nth inputs (+ i 3))
-                          (if (== (param-val inputs i param-1-mode 1)
-                                  (param-val inputs i param-2-mode 2))
+            (recur (assoc program
+                          (nth program (+ i 3))
+                          (if (== (param-val program i param-1-mode 1)
+                                  (param-val program i param-2-mode 2))
                             1
                             0))
                    (+ i 4)
@@ -378,8 +375,8 @@
           (recur nil nil true))))))
 
 (defn intcode-csv-5
-  [ints-csv input]  
-  (intcode-5 (vec (map #(Integer/parseInt %) (str/split ints-csv #","))) input))
+  [program-csv input]  
+  (intcode-5 (vec (map #(Integer/parseInt %) (str/split program-csv #","))) input))
 
 (def solution-5-input "3,225,1,225,6,6,1100,1,238,225,104,0,1101,34,7,225,101,17,169,224,1001,224,-92,224,4,224,1002,223,8,223,1001,224,6,224,1,224,223,223,1102,46,28,225,1102,66,83,225,2,174,143,224,1001,224,-3280,224,4,224,1002,223,8,223,1001,224,2,224,1,224,223,223,1101,19,83,224,101,-102,224,224,4,224,102,8,223,223,101,5,224,224,1,223,224,223,1001,114,17,224,1001,224,-63,224,4,224,1002,223,8,223,1001,224,3,224,1,223,224,223,1102,60,46,225,1101,7,44,225,1002,40,64,224,1001,224,-1792,224,4,224,102,8,223,223,101,4,224,224,1,223,224,223,1101,80,27,225,1,118,44,224,101,-127,224,224,4,224,102,8,223,223,101,5,224,224,1,223,224,223,1102,75,82,225,1101,40,41,225,1102,22,61,224,1001,224,-1342,224,4,224,102,8,223,223,1001,224,6,224,1,223,224,223,102,73,14,224,1001,224,-511,224,4,224,1002,223,8,223,101,5,224,224,1,224,223,223,4,223,99,0,0,0,677,0,0,0,0,0,0,0,0,0,0,0,1105,0,99999,1105,227,247,1105,1,99999,1005,227,99999,1005,0,256,1105,1,99999,1106,227,99999,1106,0,265,1105,1,99999,1006,0,99999,1006,227,274,1105,1,99999,1105,1,280,1105,1,99999,1,225,225,225,1101,294,0,0,105,1,0,1105,1,99999,1106,0,300,1105,1,99999,1,225,225,225,1101,314,0,0,106,0,0,1105,1,99999,1008,677,677,224,1002,223,2,223,1006,224,329,1001,223,1,223,1007,226,226,224,1002,223,2,223,1005,224,344,101,1,223,223,1008,226,226,224,1002,223,2,223,1006,224,359,101,1,223,223,8,226,677,224,102,2,223,223,1006,224,374,101,1,223,223,1107,677,226,224,1002,223,2,223,1005,224,389,101,1,223,223,1008,677,226,224,102,2,223,223,1006,224,404,1001,223,1,223,1108,677,677,224,102,2,223,223,1005,224,419,1001,223,1,223,1107,677,677,224,102,2,223,223,1006,224,434,1001,223,1,223,1108,226,677,224,1002,223,2,223,1006,224,449,101,1,223,223,8,677,226,224,1002,223,2,223,1005,224,464,101,1,223,223,108,226,677,224,102,2,223,223,1005,224,479,1001,223,1,223,1107,226,677,224,102,2,223,223,1005,224,494,101,1,223,223,108,677,677,224,1002,223,2,223,1005,224,509,1001,223,1,223,7,677,226,224,1002,223,2,223,1006,224,524,101,1,223,223,1007,677,677,224,1002,223,2,223,1006,224,539,1001,223,1,223,107,226,226,224,102,2,223,223,1006,224,554,101,1,223,223,107,677,677,224,102,2,223,223,1006,224,569,1001,223,1,223,1007,226,677,224,1002,223,2,223,1006,224,584,101,1,223,223,108,226,226,224,102,2,223,223,1006,224,599,1001,223,1,223,7,226,226,224,102,2,223,223,1006,224,614,1001,223,1,223,8,226,226,224,1002,223,2,223,1006,224,629,1001,223,1,223,7,226,677,224,1002,223,2,223,1005,224,644,101,1,223,223,1108,677,226,224,102,2,223,223,1006,224,659,101,1,223,223,107,226,677,224,102,2,223,223,1006,224,674,1001,223,1,223,4,223,99,226")
 
@@ -399,13 +396,28 @@
           {}
           (line-seq (BufferedReader. (StringReader. orbital-map-str)))))
 
-(defn count-orbits
-  [object orbital-map]
+(defn count-orbits-to-parent
+  [object parent-pred orbital-map]
   (loop [count 0
          parent (get orbital-map object)]
-    (if (nil? parent)
+    (if (parent-pred parent) 
       count
       (recur (inc count) (get orbital-map parent)))))
+
+(defn parents
+  [obj orbital-map]
+  (loop [parents []
+         parent (get orbital-map obj)]
+    (if (nil? parent)
+      parents
+      (recur (conj parents parent) (get orbital-map parent)))))
+
+(defn common-parent
+  [obj1 obj2 orbital-map]
+  (first (for [parent1 (parents obj1 orbital-map)
+               parent2 (parents obj2 orbital-map)
+               :when (= parent1 parent2)]
+           parent1)))
 
 (defn count-all-orbits
   [orbital-map]
@@ -413,7 +425,7 @@
          objects (keys orbital-map)]
     (if (empty? objects)
       count
-      (recur (+ count (count-orbits (first objects) orbital-map)) (rest objects)))))
+      (recur (+ count (count-orbits-to-parent (first objects) nil? orbital-map)) (rest objects)))))
 
 (defn run-solution-6-p1
   []
@@ -424,5 +436,198 @@
   )
 
 (defn run-solution-6-p2
+  []  
+  (let [orbital-map (orbital-map-str->map (slurp (resource "orbital_map.txt")))
+        common-parent (common-parent "SAN" "YOU" orbital-map)]
+    (+ (count-orbits-to-parent "SAN" #(= % common-parent) orbital-map)
+       (count-orbits-to-parent "YOU" #(= % common-parent) orbital-map))))
+
+(defn phase-setting-permutations
+  [range-start range-end]
+  (for [a (range range-start range-end)
+        b (range range-start range-end)
+        c (range range-start range-end)
+        d (range range-start range-end)
+        e (range range-start range-end)
+        :let [v [a b c d e]
+              sv (set v)]
+        :when (== (count sv) 5)]
+    v))
+
+(defn intcode-7
+  [program phase-setting input instruction-pointer output-value]  
+  (loop [program program
+         terminate false
+         halted false
+         i instruction-pointer
+         phase-setting phase-setting
+         input input
+         output-value output-value]
+    (if terminate
+      [program output-value halted i]
+      (let [opcode-str (str (nth program i))
+            opcode-chars (seq opcode-str)]        
+        (cond
+          (str/ends-with? opcode-str "1") ; add
+          (let [[param-1-mode param-2-mode] (param-modes opcode-chars)]
+            (recur (perform-op program i + param-1-mode param-2-mode)
+                   false
+                   false
+                   (+ i 4)
+                   phase-setting
+                   input
+                   output-value))
+          
+          (str/ends-with? opcode-str "2") ; mult
+          (let [[param-1-mode param-2-mode] (param-modes opcode-chars)]
+            (recur (perform-op program i * param-1-mode param-2-mode)
+                   false
+                   false
+                   (+ i 4)
+                   phase-setting
+                   input
+                   output-value))
+
+          (str/ends-with? opcode-str "3") ; read an input val          
+          (recur (assoc program
+                        (nth program (+ i 1))
+                        (if phase-setting phase-setting input))
+                 false
+                 false
+                 (+ i 2)
+                 nil
+                 input
+                 output-value)
+
+          (str/ends-with? opcode-str "4") ; write
+          (let [[param-1-mode] (param-modes opcode-chars)]
+            (recur program
+                   true
+                   false
+                   (+ i 2)
+                   phase-setting
+                   input
+                   (param-val program i param-1-mode 1)))
+
+          (str/ends-with? opcode-str "5") ; jump-if-true
+          (let [[param-1-mode param-2-mode] (param-modes opcode-chars)]
+            (let [jump-test (param-val program i param-1-mode 1)]
+              (if (not (== jump-test 0))
+                (recur program
+                       false
+                       false
+                       (param-val program i param-2-mode 2)
+                       phase-setting
+                       input
+                       output-value)
+                (recur program
+                       false
+                       false
+                       (+ i 3)
+                       phase-setting
+                       input
+                       output-value))))          
+
+          (str/ends-with? opcode-str "6") ; jump-if-false
+          (let [[param-1-mode param-2-mode] (param-modes opcode-chars)]
+            (let [jump-test (param-val program i param-1-mode 1)]
+              (if (== jump-test 0)
+                (recur program
+                       false
+                       false
+                       (param-val program i param-2-mode 2)
+                       phase-setting
+                       input
+                       output-value)
+                (recur program
+                       false
+                       false
+                       (+ i 3)
+                       phase-setting
+                       input
+                       output-value))))          
+
+          (str/ends-with? opcode-str "7") ; less than
+          (let [[param-1-mode param-2-mode] (param-modes opcode-chars)]            
+            (recur (assoc program                          
+                          (nth program (+ i 3))
+                          (if (< (param-val program i param-1-mode 1)
+                                 (param-val program i param-2-mode 2))
+                            1
+                            0))
+                   false
+                   false
+                   (+ i 4)                   
+                   phase-setting
+                   input
+                   output-value))
+
+          (str/ends-with? opcode-str "8") ; equals
+          (let [[param-1-mode param-2-mode] (param-modes opcode-chars)]            
+            (recur (assoc program                          
+                          (nth program (+ i 3))
+                          (if (== (param-val program i param-1-mode 1)
+                                  (param-val program i param-2-mode 2))
+                            1
+                            0))
+                   false
+                   false
+                   (+ i 4)                   
+                   phase-setting
+                   input
+                   output-value))
+
+          (str/ends-with? opcode-str "99")
+          (recur program true true nil nil nil output-value))))))
+
+(defn thruster-signal
+  [amp-program phase-setting-sequence]
+  (let [amp-a {:amp :a :prog amp-program :ptr 0 :phase-setting (nth phase-setting-sequence 0) :input 0   :halted false :output nil :next-amp :b}
+        amp-b {:amp :b :prog amp-program :ptr 0 :phase-setting (nth phase-setting-sequence 1) :input nil :halted false :output nil :next-amp :c}
+        amp-c {:amp :c :prog amp-program :ptr 0 :phase-setting (nth phase-setting-sequence 2) :input nil :halted false :output nil :next-amp :d}
+        amp-d {:amp :d :prog amp-program :ptr 0 :phase-setting (nth phase-setting-sequence 3) :input nil :halted false :output nil :next-amp :e}
+        amp-e {:amp :e :prog amp-program :ptr 0 :phase-setting (nth phase-setting-sequence 4) :input nil :halted false :output nil :next-amp :a}]
+    (loop [amp-programs {:a amp-a, :b amp-b, :c amp-c, :d amp-d, :e amp-e}
+           current-amp-key :a]      
+      (let [current-amp (get amp-programs current-amp-key)]                                        
+        (if (:halted current-amp)
+          (if (= current-amp-key :e)
+            (:output current-amp)
+            (recur amp-programs (:next-amp current-amp)))
+          (let [[result-program output halted inst-ptr] (intcode-7 (:prog current-amp)
+                                                                   (:phase-setting current-amp)                                                                   
+                                                                   (:input current-amp)
+                                                                   (:ptr current-amp)
+                                                                   (:output current-amp))                
+                amp-programs (-> amp-programs
+                                 (assoc-in [current-amp-key :output] output)
+                                 (assoc-in [current-amp-key :prog] result-program)
+                                 (assoc-in [current-amp-key :ptr] inst-ptr)
+                                 (assoc-in [current-amp-key :halted] halted)
+                                 (assoc-in [current-amp-key :phase-setting] nil)
+                                 (assoc-in [(:next-amp current-amp) :input] output))]            
+            (recur amp-programs (:next-amp current-amp)))))))))
+
+(defn max-thruster-signal
+  [amp-program phase-setting-range-start phase-setting-range-end]
+  (loop [phase-setting-permutations (phase-setting-permutations phase-setting-range-start phase-setting-range-end)         
+         max-thruster-signal 0]
+    (if (empty? phase-setting-permutations)
+      max-thruster-signal
+      (let [phase-setting (first phase-setting-permutations)
+            thruster-signal (thruster-signal amp-program phase-setting)]
+        (if (> thruster-signal max-thruster-signal)
+          (recur (next phase-setting-permutations) thruster-signal)
+          (recur (next phase-setting-permutations) max-thruster-signal))))))
+
+(def day-7-program "3,8,1001,8,10,8,105,1,0,0,21,34,59,68,85,102,183,264,345,426,99999,3,9,101,3,9,9,102,3,9,9,4,9,99,3,9,1002,9,4,9,1001,9,2,9,1002,9,2,9,101,5,9,9,102,5,9,9,4,9,99,3,9,1001,9,4,9,4,9,99,3,9,101,3,9,9,1002,9,2,9,1001,9,5,9,4,9,99,3,9,1002,9,3,9,1001,9,5,9,102,3,9,9,4,9,99,3,9,1001,9,1,9,4,9,3,9,1001,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,101,1,9,9,4,9,99,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,101,2,9,9,4,9,99,3,9,1001,9,1,9,4,9,3,9,101,1,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,1001,9,1,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,1,9,4,9,99,3,9,1002,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,101,2,9,9,4,9,99,3,9,1001,9,1,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,102,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,102,2,9,9,4,9,3,9,101,2,9,9,4,9,99")
+
+(defn run-solution-7-p1
   []
-  )
+  (max-thruster-signal (vec (map #(Integer/parseInt %) (str/split day-7-program #","))) 0 5))
+
+(defn run-solution-7-p2
+  []
+  (max-thruster-signal (vec (map #(Integer/parseInt %) (str/split day-7-program #","))) 5 10))
+
+
