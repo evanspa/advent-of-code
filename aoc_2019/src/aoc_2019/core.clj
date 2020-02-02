547e6e711550f2c8a2e516556611f540b24dd59a
Paul Evans <evansp2@gmail.com>
2020-02-02 08:21:15 -0500

    day 11 solution

--- aoc_2019/src/aoc_2019/core.clj
+++ aoc_2019/src/aoc_2019/core.clj
@@ -652,7 +652,7 @@
   []
   (run-program program-9 1))
 
-(defn run-solution-9-p2
+(defn run-solution-9-p2>
   []
   (run-program program-9 2))
 
@@ -865,7 +865,30 @@
 
 (def day-11-p1-input "3,8,1005,8,328,1106,0,11,0,0,0,104,1,104,0,3,8,102,-1,8,10,101,1,10,10,4,10,108,1,8,10,4,10,101,0,8,28,1006,0,13,3,8,102,-1,8,10,101,1,10,10,4,10,1008,8,1,10,4,10,1002,8,1,54,1,1103,9,10,1006,0,97,2,1003,0,10,1,105,6,10,3,8,102,-1,8,10,1001,10,1,10,4,10,1008,8,1,10,4,10,1001,8,0,91,3,8,102,-1,8,10,101,1,10,10,4,10,1008,8,0,10,4,10,102,1,8,113,2,109,5,10,1006,0,96,1,2,5,10,3,8,1002,8,-1,10,101,1,10,10,4,10,1008,8,0,10,4,10,102,1,8,146,2,103,2,10,1006,0,69,2,9,8,10,1006,0,25,3,8,102,-1,8,10,1001,10,1,10,4,10,1008,8,0,10,4,10,101,0,8,182,3,8,1002,8,-1,10,101,1,10,10,4,10,108,1,8,10,4,10,1001,8,0,203,2,5,9,10,1006,0,0,2,6,2,10,3,8,102,-1,8,10,101,1,10,10,4,10,108,1,8,10,4,10,1002,8,1,236,2,4,0,10,3,8,1002,8,-1,10,1001,10,1,10,4,10,1008,8,0,10,4,10,1002,8,1,263,2,105,9,10,1,103,15,10,1,4,4,10,2,109,7,10,3,8,1002,8,-1,10,101,1,10,10,4,10,1008,8,0,10,4,10,1001,8,0,301,1006,0,63,2,105,6,10,101,1,9,9,1007,9,1018,10,1005,10,15,99,109,650,104,0,104,1,21102,387508441116,1,1,21102,1,345,0,1106,0,449,21102,1,387353256852,1,21102,1,356,0,1105,1,449,3,10,104,0,104,1,3,10,104,0,104,0,3,10,104,0,104,1,3,10,104,0,104,1,3,10,104,0,104,0,3,10,104,0,104,1,21101,179410308315,0,1,21102,1,403,0,1106,0,449,21101,206199495827,0,1,21102,414,1,0,1105,1,449,3,10,104,0,104,0,3,10,104,0,104,0,21102,718086758760,1,1,21102,1,437,0,1105,1,449,21101,838429573908,0,1,21102,448,1,0,1106,0,449,99,109,2,21202,-1,1,1,21102,1,40,2,21102,480,1,3,21101,470,0,0,1105,1,513,109,-2,2105,1,0,0,1,0,0,1,109,2,3,10,204,-1,1001,475,476,491,4,0,1001,475,1,475,108,4,475,10,1006,10,507,1102,0,1,475,109,-2,2106,0,0,0,109,4,2101,0,-1,512,1207,-3,0,10,1006,10,530,21101,0,0,-3,21202,-3,1,1,21201,-2,0,2,21102,1,1,3,21102,549,1,0,1105,1,554,109,-4,2106,0,0,109,5,1207,-3,1,10,1006,10,577,2207,-4,-2,10,1006,10,577,22102,1,-4,-4,1106,0,645,22102,1,-4,1,21201,-3,-1,2,21202,-2,2,3,21101,596,0,0,1106,0,554,22101,0,1,-4,21102,1,1,-1,2207,-4,-2,10,1006,10,615,21101,0,0,-1,22202,-2,-1,-2,2107,0,-3,10,1006,10,637,21201,-1,0,1,21101,637,0,0,106,0,512,21202,-2,-1,-2,22201,-4,-2,-4,109,-5,2106,0,0")
 
-(defn run-program
+(defn next-orientation
+  [orientation turn-direction]
+  (case orientation
+    :up (case turn-direction 0 :left 1 :right)
+    :right (case turn-direction 0 :up 1 :down)
+    :down (case turn-direction 0 :right 1 :left)
+    :left (case turn-direction 0 :down 1 :up)))
+
+(defn next-position
+  [orientation [current-pos-x current-pos-y]]
+  (case orientation
+    :up [current-pos-x (inc current-pos-y)]
+    :right [(inc current-pos-x) current-pos-y]
+    :down [current-pos-x (dec current-pos-y)]
+    :left [(dec current-pos-x) current-pos-y]))
+
+(defn hull-color
+  [hull position]
+  (let [color (get hull position)]
+    (if (nil? color)
+      0 ; default is 0 (black)
+      color)))
+
+(defn paint-hull
   "Executes the intcode computer using the given program and input."
   [prog-str input]  
   (loop [prog (indexed (vec (map #(str->bigint %) (str/split prog-str #","))))
@@ -874,15 +897,98 @@
          output-value nil
          relative-base 0
          paused false
-         halted false]
-    (if halted
-      nil 
-      (if paused
-        (do
-          (println output-value)
-          #_(let [[program output-value paused halted i relative-base] (intcode prog nil input instr-ptr output-value relative-base)]
-            (recur program input i output-value relative-base paused halted)))                                        
-        (let [[program output-value paused halted i relative-base] (intcode prog nil input instr-ptr output-value relative-base)]
-          (recur program input i output-value relative-base paused halted))))))
+         halted false
+         orientation :up
+         position [0 0]
+         painted-hull {}
+         pause-count 0
+         min-x 0
+         max-x 0
+         min-y 0
+         max-y 0]    
+    (if halted      
+      [painted-hull min-x max-x min-y max-y]
+      (let [[prog output-value paused halted instr-ptr relative-base] (intcode prog nil input instr-ptr output-value relative-base)]
+        (if paused
+          (if (== 0 pause-count) 
+            (recur prog ; output-value is color to paint current panel                   
+                   input
+                   instr-ptr
+                   output-value
+                   relative-base
+                   paused
+                   halted
+                   orientation
+                   position
+                   (assoc painted-hull position output-value)
+                   (inc pause-count)
+                   min-x
+                   max-x
+                   min-y
+                   max-y)
+            (let [next-orientation (next-orientation orientation output-value) ; output-value is direction to turn
+                  next-position (next-position next-orientation position)
+                  [next-pos-x next-pos-y] next-position] 
+              (recur prog 
+                     (hull-color painted-hull next-position)                   
+                     instr-ptr
+                     output-value
+                     relative-base
+                     paused
+                     halted
+                     next-orientation
+                     next-position
+                     painted-hull                   
+                     0
+                     (if (< next-pos-x min-x) next-pos-x min-x)
+                     (if (> next-pos-x max-x) next-pos-x max-x)
+                     (if (< next-pos-y min-y) next-pos-y min-y)
+                     (if (> next-pos-y max-y) next-pos-y max-y))))        
+          (recur prog ; program is not paused            
+                 (hull-color painted-hull position)
+                 instr-ptr
+                 output-value
+                 relative-base
+                 paused
+                 halted
+                 orientation
+                 position
+                 painted-hull
+                 pause-count
+                 min-x
+                 max-x
+                 min-y
+                 max-y))))))
+
+(defn run-solution-11-p1
+  []
+  (let [[hull _ _ _ _] (paint-hull day-11-p1-input 0)]    
+    (count hull)))
+
+(defn run-solution-11-p2
+  []
+  (let [[hull min-x max-x min-y max-y] (paint-hull day-11-p1-input 1)
+        x-range (range min-x (inc max-x))
+        y-range (reverse (range min-y (inc max-y)))]    
+    (doseq [y y-range]
+      (doseq [x x-range]
+        (let [hull-color (hull-color hull [x y])]          
+          (print (if (== 0 hull-color) "." "#"))))
+      (print "\n"))))
+
+(def xyz-regex #"<x=(.*), y=(.*), z=(.*)>")
+
+(defn parse-xyz-vals
+  [s]
+  (map #(Integer/parseInt %) (rest (re-matches xyz-regex s))))
+
+(def day-12-input "<x=3, y=2, z=-6>\n<x=-13, y=18, z=10>\n<x=-8, y=-1, z=13>\n<x=5, y=10, z=4>\n")
+
+(defn str->xyz-vectors
+  [xyz-vals-str]
+  (map parse-xyz-vals (line-seq (BufferedReader. (StringReader. xyz-vals-str)))))
+
+
+
 
 
